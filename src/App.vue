9721364db0bc8643082b8d3d0f86fa045b4d2f4c
xiaoxinyi <564881987@qq.com>
2019-02-19 09:22:42 +0800

    提交代码

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+      <router-view name="main"></router-view>
+      <router-view name="slidebar"></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+  import PostList from './components/PostList'
+export default {
+  name: 'App',
+  components:{
+    Header,PostList
+  }
+}
+</script>
+
+<style>
+
+</style>
